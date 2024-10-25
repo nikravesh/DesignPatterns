1271666e3a8374d594d2af9b6b50efb879a6dd7d
Alireza Nikravesh <a.z.nikravesh@gmail.com>
2024-10-25 20:21:10 +0330

    added mogoDB product

---
+++ 01-Creationals/AbstractFactory/Creationals.AbstractFactory/RealWordExample/ConcreteClasses/MongoDBCommand.cs
@@ -0,0 +1,7 @@
+﻿using Creationals.AbstractFactory.RealWordExample.SuperClasses;
+
+namespace Creationals.AbstractFactory.RealWordExample.ConcreteClasses;
+
+public class MongoDBCommand : IMyDbCommand
+{
+    public void Execute() => Console.WriteLine("MongoDB command executed!");
---
+++ 01-Creationals/AbstractFactory/Creationals.AbstractFactory/RealWordExample/ConcreteClasses/MongoDBConnection.cs
@@ -0,0 +1,9 @@
+﻿using Creationals.AbstractFactory.RealWordExample.SuperClasses;
+
+namespace Creationals.AbstractFactory.RealWordExample.ConcreteClasses;
+public class MongoDBConnection : IMyDbConnection
+{
+    public void Close() => Console.WriteLine("MongoDB connection closed!");
+
+    public void Open() => Console.WriteLine("MongoDB connection opened!");
+}
