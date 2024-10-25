36df3b37e2a1a20983c89ddafca5adcaf6717e21
Alireza Nikravesh <a.z.nikravesh@gmail.com>
2024-10-25 20:16:40 +0330

    Added MySQL product

---
+++ 01-Creationals/AbstractFactory/Creationals.AbstractFactory/RealWordExample/ConcreteClasses/MySQLCommand.cs
@@ -0,0 +1,8 @@
+﻿using Creationals.AbstractFactory.RealWordExample.SuperClasses;
+
+namespace Creationals.AbstractFactory.RealWordExample.ConcreteClasses;
+
+public class MySQLCommand : IMyDbCommand
+{
+    public void Execute() => Console.WriteLine("MySQL command executed!");
+}
---
+++ 01-Creationals/AbstractFactory/Creationals.AbstractFactory/RealWordExample/ConcreteClasses/MySQLConnection.cs
@@ -0,0 +1,9 @@
+﻿using Creationals.AbstractFactory.RealWordExample.SuperClasses;
+
+namespace Creationals.AbstractFactory.RealWordExample.ConcreteClasses;
+public class MySQLConnection : IMyDbConnection
+{
+    public void Close() => Console.WriteLine("MySQL connection closed!");
+
+    public void Open() => Console.WriteLine("MySQL connection opened!");
+}
