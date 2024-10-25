abb5b56b770e81dc289dc2d0b07a7cb6fc918221
Alireza Nikravesh <a.z.nikravesh@gmail.com>
2024-10-25 20:13:27 +0330

    added sql server product

---
+++ 01-Creationals/AbstractFactory/Creationals.AbstractFactory/RealWordExample/ConcreteClasses/SqlServerCommand.cs
@@ -0,0 +1,7 @@
+﻿using Creationals.AbstractFactory.RealWordExample.SuperClasses;
+
+namespace Creationals.AbstractFactory.RealWordExample.ConcreteClasses;
+public class SqlServerCommand : IMyDbCommand
+{
+    public void Execute() => Console.WriteLine("sql server command executed!!");
+}
---
+++ 01-Creationals/AbstractFactory/Creationals.AbstractFactory/RealWordExample/ConcreteClasses/SqlServerConnection.cs
@@ -0,0 +1,10 @@
+﻿using Creationals.AbstractFactory.RealWordExample.SuperClasses;
+
+namespace Creationals.AbstractFactory.RealWordExample.ConcreteClasses;
+
+public class SqlServerConnection : IMyDbConnection
+{
+    public void Close() => Console.WriteLine("Sql server connection closed!");
+
+    public void Open() => Console.WriteLine("Sql server connection opened!");
+}
