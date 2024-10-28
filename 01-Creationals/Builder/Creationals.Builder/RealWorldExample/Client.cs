6956f7eff8c17fa99b688ac90387e2ab1b37aecc
Alireza Nikravesh <a.z.nikravesh@gmail.com>
2024-10-28 16:00:19 +0330

    added an example to use

--- 01-Creationals/Builder/Creationals.Builder/RealWorldExample/Client.cs
+++ 01-Creationals/Builder/Creationals.Builder/RealWorldExample/Client.cs
@@ -1,8 +1,16 @@
-﻿namespace Creationals.Builder.RealWorldExample;
+﻿using Creationals.Builder.RealWorldExample.ConcreteClasses;
+using Creationals.Builder.RealWorldExample.Director;
+using Creationals.Builder.RealWorldExample.Product;
+
+namespace Creationals.Builder.RealWorldExample;
 public class Client
 {
     public Client()
     {
-        
+        MailSender mail = new();
+        MailSenderDirector mailSenderDirector = new(mail);
+
+        mailSenderDirector.Build();
+        MailModel result = mail.GetResult();
     }
 }
--- 01-Creationals/Builder/Creationals.Builder/RealWorldExample/Director/MailSenderDirector.cs
+++ 01-Creationals/Builder/Creationals.Builder/RealWorldExample/Director/MailSenderDirector.cs
@@ -3,13 +3,20 @@
 namespace Creationals.Builder.RealWorldExample.Director;
 public class MailSenderDirector
 {
+    private readonly IMailSenderBuilder builder;
+
     public MailSenderDirector(IMailSenderBuilder builder)
+    {
+        this.builder = builder;
+    }
+
+    public void Build()
     {
         builder
             .BuildFrom("a.z.nikravesh@gmail.com")
             .BuildTo("a.z.nikravesh@outlook.com")
             .BuildSubject("Send an email")
             .BuildBody("This is a test mail")
-            .BuildHasAttachments(false);            
+            .BuildHasAttachments(false);
     }
 }
