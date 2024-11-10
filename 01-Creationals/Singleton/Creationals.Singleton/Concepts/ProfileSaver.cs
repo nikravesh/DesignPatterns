fc52dc04e92499c320a26387975e5a74453a52f0
Alireza Nikravesh <a.z.nikravesh@gmail.com>
2024-11-10 22:13:45 +0330

    added a sample of singleton

---
+++ 01-Creationals/Singleton/Creationals.Singleton/Concepts/ProfileSaver.cs
@@ -0,0 +1,30 @@
+﻿namespace Creationals.Singleton.Concepts;
+public class ProfileSaver
+{
+    private static object _lockObject = new();
+
+    private static ProfileSaver _instance;
+    public static ProfileSaver Instance
+    {
+        get
+        {
+            if (_instance is null)
+            {
+                lock (_lockObject)
+                {
+                    if(_instance is null)
+                        return _instance = new ProfileSaver();
+                    return _instance;
+                }
+            }
+            else
+                return _instance;
+        }
+    }
+
+
+
+    private ProfileSaver()
+    {
+    }
+}
